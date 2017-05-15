baa6725716176af9118e2a6734a62be9f88373c0
nicejune7 <nicejune7@hanmail.net>
2017-05-15 16:59:05 +0900

    Initial commit

---
+++ app/src/main/java/com/example/nicej/schedulehelper/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.nicej.schedulehelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("Welcome Github!");
+        setContentView(tv);
+    }
+}
